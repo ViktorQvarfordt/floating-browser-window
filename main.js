9bd979bdca67e576c5a7a2bc4fba69650ad258e9
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2025-02-22 22:23:42 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, BrowserView, globalShortcut } = require('electron')
+
+const url = process.argv[1] || 'https://google.com'
+const toggleVisibilityShortcut = process.argv[2] || 'Alt+B'
+
+let mainWindow = null
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ 
+    width: 800, 
+    height: 400,
+    frame: false,
+    alwaysOnTop: false,
+  })
+
+  const view = new BrowserView({ webPreferences: { partition: 'persist:main' } })
+  
+  mainWindow.addBrowserView(view)
+
+  view.webContents.loadURL(url)
+  
+  const updateViewBounds = () => {
+    const { width, height} = mainWindow.getBounds()
+    view.setBounds({ x: 0, y: 0, width, height })
+  }
+
+  updateViewBounds()
+
+  mainWindow.on('resize', updateViewBounds)
+
+  globalShortcut.register(toggleVisibilityShortcut, () => {
+    if (mainWindow.isFocused()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.show()
+    }
+  })
+
+  mainWindow.on('close', (event) => {
+    event.preventDefault()
+    mainWindow.hide()
+  })
+}
+
+app.whenReady().then(createWindow)
